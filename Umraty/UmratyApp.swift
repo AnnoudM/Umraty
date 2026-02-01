d0fcc3554d5fc94acf8184c7cd17a069f6df9251
AnnoudM <443201069@student.ksu.edu.sa>
2026-02-01 11:44:56 +0300

    Initial Commit

---
+++ Umraty/UmratyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmratyApp.swift
+//  Umraty
+//
+//  Created by Alanood Almozaini on 01/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct UmratyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
